3c365958e08aa44f409d65ce4b94f7035083ff08
luobo <luobo@qq.com>
2023-12-14 21:49:14 +0800

    12.14        21：49

---
+++ idea_project/data_structure/src/Leetcode/Problem231.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class Problem231 {
+    public static void main(String[] args) {
+        int a=88888888;
+        int b=45345343;
+        System.out.println(isPowerOfTwo(a));
+        System.out.println(isPowerOfTwo(b));
+    }
+    public static boolean isPowerOfTwo(int n) {
+        if(n%2==0)
+            return isPowerOfTwo(n/2);
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
