d64103efcee8f6c7384c2cce8466724cb8f6af7f
luobo <luobo@qq.com>
2024-03-08 21:37:58 +0800

    2024.3.8

---
+++ idea_project/data_structure/src/Leetcode/Problem036.java
@@ -0,0 +1,47 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class Problem036 {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> list=new Stack<>();
+        for (String t:
+             tokens) {
+            if(t=="+"){
+                Integer a=list.pop();
+                Integer b=list.pop();
+                Integer c=b+a;
+                list.push(c);
+            }
+            else if(t=="-"){
+                Integer a=list.pop();
+                Integer b=list.pop();
+                Integer c=b-a;
+                list.push(c);
+            }
+            else if(t=="*"){
+                Integer a=list.pop();
+                Integer b=list.pop();
+                Integer c=b*a;
+                list.push(c);
+            }
+            else if(t=="/"){
+                Integer a=list.pop();
+                Integer b=list.pop();
+                Integer c=b/a;
+                list.push(c);
+            }
+            else
+            {
+                list.push(Integer.parseInt(t));
+            }
+        }
+        return list.peek();
+    }
+
+    public static void main(String[] args) {
+        String[] tokens={"2","1","+","3","*"};
+        int a=evalRPN(tokens);
+        System.out.println(a);
+    }
+}
