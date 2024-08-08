6e97fdaacc4fd42bb01c2182bf214586b36d4662
wwq <2251231008@qq.com>
2024-08-08 20:58:21 +0800

    tomcat

---
+++ javaWeb/Tomcat/src/servlet/Servlet_Demo1.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class Servlet_Demo1 implements Servlet {
+    /**
+     *初始化时执行
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+    //访问时执行
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
