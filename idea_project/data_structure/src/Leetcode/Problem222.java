b5555c30997ce724384e8febcc4370b13e484e28
luobo <luobo@qq.com>
2025-01-04 14:11:53 +0800

    2025.1.4

--- idea_project/data_structure/src/Leetcode/Problem222.java
+++ idea_project/data_structure/src/Leetcode/Problem222.java
@@ -5,17 +5,23 @@ import Tree.TreeNode;
 import java.util.Stack;
 
 public class Problem222 {
-//    public int countNodes(TreeNode root) {
-//        if(root==null) return 0;
-//        Stack<TreeNode> stack=new Stack<>();
-//        TreeNode cur=root;
-//        while (!stack.isEmpty()||cur!=null){
-//            if(cur!=null){
-//                stack.push(cur);
-//                cur=cur.right;
-//            }else {
-//
-//            }
-//        }
-//    }
+    //完全二叉树的节点个数
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        int leftDepth = 0, rightDepth = 0; // 这里初始为0是有目的的，为了下面求指数方便
+        while (left != null) {  // 求左子树深度
+            left = left.left;
+            leftDepth++;
+        }
+        while (right != null) { // 求右子树深度
+            right = right.right;
+            rightDepth++;
+        }
+        if (leftDepth == rightDepth) {
+            return (2 << leftDepth) - 1; // 注意(2<<1) 相当于2^2，所以leftDepth初始为0
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
 }
